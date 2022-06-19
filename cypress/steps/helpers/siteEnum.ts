0be07cfef50b7fe8ccb95c23c33621f4accc15a6
Alexander Maximov <maximova@odysseyvc.com>
2022-06-19 22:22:29 +0300

    add wikipedia scenario

---
+++ cypress/steps/helpers/siteEnum.ts
@@ -0,0 +1,4 @@
+export enum siteName {
+  Google = "Google",
+  Wikipedia = "Wikipedia",
+}
--- cypress/steps/test.ts
+++ cypress/steps/test.ts
@@ -1,18 +1,39 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
+import { siteName } from "./helpers/siteEnum";
 
-Given(`I am on Google search page`, () =>{
-    cy.visit('https://google.com');
+let siteContext: string;
+
+Given(`I am on {string} search page`, (pageName: string) => {
+  switch (pageName.toLowerCase()) {
+    case "google":
+      cy.visit("https://google.com");
+      siteContext = siteName.Google;
+      break;
+    case "wikipedia":
+      cy.visit("https://wikipedia.org");
+      siteContext = siteName.Wikipedia;
+      break;
+    default:
+      throw new Error("unknown site, pageName: " + pageName);
+  }
 });
 
-When('I type {string} in the search field', (searchField) =>{
-    cy.findByRole('combobox', {class: '.gLFyf'})
-        .type(searchField + '{enter}')
-    
+When("I search {string} term", (searchField) => {
+  searchTerm(searchField);
 });
 
-Then('it should be ok', () =>{
-    cy.log("Then section");
-    cy.title().should("include", "Cypress");
-
+Then("title should contain {string}", (expectedTitleSubstring) => {
+  cy.log("Then section");
+  cy.title().should("include", expectedTitleSubstring);
 });
 
+Then("article header should be {string}", () => {});
+
+function searchTerm(term: string) {
+  if (siteContext == siteName.Google) {
+    cy.findByRole("combobox", { class: ".gLFyf" }).type(term + "{enter}");
+  }
+  if (siteContext == siteName.Wikipedia) {
+    cy.get("#searchInput").type(term + "{enter}");
+  }
+}
